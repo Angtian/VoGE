9c979d7f893dfcfab888b084284d7099729eb83d
Angtian Wang <63261150+Angtian@users.noreply.github.com>
2022-05-31 12:54:46 -0400

    Update __init__.py

--- VoGE/__init__.py
+++ VoGE/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 
 # from .Rasterizer import Fragments, GaussianRasterizationSettings, GaussianRasterizer, interpolate_attr, get_silhouette, to_colored_background, to_white_background
